502ab91917d38a355f4e8bb5fb3446988a48b6de
Dan Alexander <lxndrdagreat@gmail.com>
2017-08-30 12:55:14 -0400

    styles for xp table on about page

--- coffee_leaderboard/views/plusxp.py
+++ coffee_leaderboard/views/plusxp.py
@@ -1,6 +1,7 @@
 # coffee_leaderboard/views/plusxp.py
 
 from flask import Blueprint, render_template
+from coffee_leaderboard.utils import XP_TABLE
 
 
 mod = Blueprint('plusxp', __name__)
@@ -8,4 +9,4 @@ mod = Blueprint('plusxp', __name__)
 
 @mod.route('/')
 def about_plusxp():
-    return render_template('plusxp/about.html')
+    return render_template('plusxp/about.html', xp_table=XP_TABLE)

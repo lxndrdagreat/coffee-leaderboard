b0ea73618a8e1d1ba3769235979c4bdf84ad20eb
Dan Alexander <lxndrdagreat@gmail.com>
2017-08-30 11:08:04 -0400

    Adding information for plusxp

--- coffee_leaderboard/__init__.py
+++ coffee_leaderboard/__init__.py
@@ -7,6 +7,8 @@ app.config.from_object('config')
 # set up views
 from coffee_leaderboard.views import leaderboard
 from coffee_leaderboard.views import profile
+from coffee_leaderboard.views import plusxp
 
 app.register_blueprint(leaderboard.mod)
 app.register_blueprint(profile.mod, url_prefix='/profile')
+app.register_blueprint(plusxp.mod, url_prefix='/plusxp')
--- coffee_leaderboard/views/plusxp.py
+++ coffee_leaderboard/views/plusxp.py
@@ -1,1 +1,11 @@
 # coffee_leaderboard/views/plusxp.py
+
+from flask import Blueprint, render_template
+
+
+mod = Blueprint('plusxp', __name__)
+
+
+@mod.route('/')
+def about_plusxp():
+    return render_template('plusxp/about.html')
